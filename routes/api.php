52d5392d4dc770887559fea81b35ccfa945f7145
Luis <luisjin9@gmail.com>
2022-03-04 08:57:00 +0100

    imported

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/users', [UserController::class, 'allUsers']);
+Route::get('/users/{id}', [UserController::class, 'getUserById']);
+Route::post('/something', [UserController::class, 'postSomething']);
